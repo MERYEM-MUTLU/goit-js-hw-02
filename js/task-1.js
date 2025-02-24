473351896b0c023947eeae8da2ae09e724df890f
meryem_mutlu <meryemmutlu.cm@gmail.com>
2025-02-24 21:55:55 +0300

    1.bölüm düzeltmesi

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   // toplam fiyat
-  const totalPrice = quantity * pricePerDroid;
+  let totalPrice = quantity * pricePerDroid;
 
   //Kredi durumu
   if (totalPrice > customerCredits) {
@@ -11,8 +11,8 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 //sonuçları gör
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
 console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
